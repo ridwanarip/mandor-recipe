07e86cedc59fd556800de766c13f2a8b244e10a0
Ridwan Arip <arief_09@ymail.com>
2018-03-24 00:53:58 +0700

    edit index.html

---
+++ src/test/java/com/mandor/domain/CategoryTest.java
@@ -0,0 +1,24 @@
+package com.mandor.domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CategoryTest {
+
+	@Test
+	public void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRecipes() {
+		fail("Not yet implemented");
+	}
+
+}
